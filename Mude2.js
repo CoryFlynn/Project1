4b6e102bcd674c1a58d219cf7add06ca79dd921d
CoryFlynn <45837622+CoryFlynn@users.noreply.github.com>
2019-02-27 14:35:26 -0700

    Add files via upload
    Sound!

--- Mude2.js
+++ Mude2.js
@@ -1,10 +1,71 @@
+$("#P1").click(function() {
+  var omniOsc = new Tone.OmniOscillator("D4", "pwm");
+  var ampEnv = new Tone.AmplitudeEnvelope({
+	"attack": 0.1,
+	"decay": 0.2,
+	"sustain": 1.0,
+	"release": 0.8
+  }).toMaster();
+  var osc = new Tone.Oscillator().connect(ampEnv).start();
+  ampEnv.triggerAttackRelease("8t");
+});
 
-const synth = new Tone.MembraneSynth().toMaster();
-const synthPart = new Tone.Sequence(
-  function(time, note) {
-    synth.triggerAttackRelease(note, "10hz", time);
-  },
-  notes,
-  "4n"
-);
-synthPart.start();
+$("#P2").click(function() {
+  var omniOsc = new Tone.OmniOscillator("C#4", "pwm");
+  var ampEnv = new Tone.AmplitudeEnvelope({
+	"attack": 0.1,
+	"decay": 0.2,
+	"sustain": 1.0,
+	"release": 0.8
+  }).toMaster();
+  var osc = new Tone.Oscillator().connect(ampEnv).start();
+  ampEnv.triggerAttackRelease("8t");
+});
+
+$("#P3").click(function() {
+  var omniOsc = new Tone.OmniOscillator("Eb2", "pwm");
+  var ampEnv = new Tone.AmplitudeEnvelope({
+	"attack": 0.1,
+	"decay": 0.2,
+	"sustain": 1.0,
+	"release": 0.8
+  }).toMaster();
+  var osc = new Tone.Oscillator().connect(ampEnv).start();
+  ampEnv.triggerAttackRelease("8t");
+});
+
+$("#P4").click(function() {
+  var omniOsc = new Tone.OmniOscillator("C2", "pwm");
+  var ampEnv = new Tone.AmplitudeEnvelope({
+	"attack": 0.1,
+	"decay": 0.2,
+	"sustain": 1.0,
+	"release": 0.8
+  }).toMaster();
+  var osc = new Tone.Oscillator().connect(ampEnv).start();
+  ampEnv.triggerAttackRelease("8t");
+});
+
+$("#P5").click(function() {
+  var omniOsc = new Tone.OmniOscillator("C2", "pwm");
+  var ampEnv = new Tone.AmplitudeEnvelope({
+	"attack": 0.1,
+	"decay": 0.2,
+	"sustain": 1.0,
+	"release": 0.8
+  }).toMaster();
+  var osc = new Tone.Oscillator().connect(ampEnv).start();
+  ampEnv.triggerAttackRelease("8t");
+});
+
+$("#P6").click(function() {
+  var omniOsc = new Tone.OmniOscillator("C2", "pwm");
+  var ampEnv = new Tone.AmplitudeEnvelope({
+	"attack": 0.1,
+	"decay": 0.2,
+	"sustain": 1.0,
+	"release": 0.8
+  }).toMaster();
+  var osc = new Tone.Oscillator().connect(ampEnv).start();
+  ampEnv.triggerAttackRelease("8t");
+});
